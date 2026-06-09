08b6c7b9c2c2612b9172426f3188a4855e758b5d
EladB1 <EladB1@users.noreply.github.com>
2026-06-09 19:00:37 -0400

    Removing build script

--- scripts/build.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Building compiler executable...'
-go build -o the ./cmd/the
-if [[ $? -eq 0 ]]; then
-    echo 'Successfully built executable';
-else
-    echo 'Failed to build executable';
-fi
